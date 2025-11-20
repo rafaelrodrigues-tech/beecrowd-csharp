26bc16eb269199fcae1b119610f7ea0772de5c33
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-11-20 19:49:19 -0300

    feat: adicionar Uri1000 e corrigir .gitignore

---
+++ exercicios/uri1000/Uri1000/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uri1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
