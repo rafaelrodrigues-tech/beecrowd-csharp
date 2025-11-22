13ab10f880a7930bd303775c086ddbac1da63c7e
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-11-22 11:45:53 -0300

    Hello Word!

--- exercicios/uri1000/Uri1000/Program.cs
+++ exercicios/uri1000/Uri1000/Program.cs
@@ -1,28 +1,11 @@
 ﻿using System;
-using System.Globalization;
-using System.Runtime.InteropServices;
-
-namespace Uri1000
+namespace uri1000
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double notaUM, notaDois, notaFinal;
-            notaUM = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            notaDois = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            notaFinal = notaUM + notaDois;
-
-            if (notaFinal > 60.00)
-            {
-                Console.WriteLine($"NOTA FINAL = {notaFinal.ToString("F1",CultureInfo.InvariantCulture)}");
-            }
-            else
-            {
-                Console.WriteLine($"NOTA FINAL = {notaFinal.ToString("F1", CultureInfo.InvariantCulture)}");
-                Console.WriteLine("REPROVADO");
-            }
-                Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
