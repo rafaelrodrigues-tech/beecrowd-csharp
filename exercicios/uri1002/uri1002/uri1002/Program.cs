0f2cf8b5ccbebda930b9b991b99497b2a29c9934
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-11-22 12:01:04 -0300

    Área do Circulo

---
+++ exercicios/uri1002/uri1002/uri1002/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.ComponentModel;
+
+namespace LogicaCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, area;
+
+            pi = 3.14159;
+            raio = double.Parse(Console.ReadLine());
+
+            area = pi * (Math.Pow(raio, 2));
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
