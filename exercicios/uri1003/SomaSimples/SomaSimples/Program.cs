03e693e4fc77089fc3c23ecc13ebb41dc5683fba
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-11-24 21:19:22 -0300

    Commit da Segunda

---
+++ exercicios/uri1003/SomaSimples/SomaSimples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SomaSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, SOMA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+        }
+    }
+}
---
+++ exercicios/uri1005/MediaUm/MediaUm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MediaUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, PesoA, PesoB, MEDIA;
+
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            PesoA = 3.5;
+            PesoB = 7.5;
+            MEDIA = (A * PesoA + B * PesoB) / (PesoA + PesoB);
+
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
