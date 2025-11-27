d1c90e6a21a9465e1cdab84aa1a8adc5c41a2bf3
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-11-26 22:32:33 -0300

    Tempo de Jogo

---
+++ exercicios/uri1046/TempoJogo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class uri1046
+{
+
+    static void Main(string[] args)
+    {
+        int HoraInicial, HoraFinal, Res;
+        string[] vet = Console.ReadLine().Split(' ');
+        HoraInicial = int.Parse(vet[0]);
+        HoraFinal = int.Parse(vet[1]);
+
+        if (HoraInicial < HoraFinal)
+        {
+            Res = HoraFinal - HoraInicial;
+            Console.WriteLine($"O JOGO DUROU {Res} HORA(S)");
+        }
+        else
+        {
+            Res = (24 + HoraFinal) - HoraInicial;
+            Console.WriteLine($"O JOGO DUROU {Res} HORA(S)");
+        }
+    }
+}
