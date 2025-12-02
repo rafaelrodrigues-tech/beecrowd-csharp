3d74e2d65584ed3d1ae6ef1c0ef47e08f5645477
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-02 19:57:20 -0300

    Primeiro commit de Dezembro

---
+++ exercicios/uri1040/Media3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        float N1, N2, N3, N4, media;
+        string[] Notas = Console.ReadLine().Split(' ');
+        N1 = float.Parse(Notas[0]);
+        N2 = float.Parse(Notas[1]);
+        N3 = float.Parse(Notas[2]);
+        N4 = float.Parse(Notas[3]);
+
+        media = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / (2 + 3 + 4 + 1);
+
+        Console.WriteLine(media);
+        Console.ReadKey();
+    }
+
+}
