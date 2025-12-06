1eff426de0facee3d558c13df7f01d7ddc05810f
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-06 19:33:14 -0300

    Adiciona uri1040, 1041 e 1045

--- exercicios/uri1040/Media3/Program.cs
+++ exercicios/uri1040/Media3/Program.cs
@@ -5,16 +5,48 @@ class URI
 
     static void Main(string[] args)
     {
-        float N1, N2, N3, N4, media;
+        float N1, N2, N3, N4, media, exame, MediaExame;
         string[] Notas = Console.ReadLine().Split(' ');
-        N1 = float.Parse(Notas[0]);
-        N2 = float.Parse(Notas[1]);
-        N3 = float.Parse(Notas[2]);
-        N4 = float.Parse(Notas[3]);
+        N1 = float.Parse(Notas[0], CultureInfo.InvariantCulture);
+        N2 = float.Parse(Notas[1], CultureInfo.InvariantCulture);
+        N3 = float.Parse(Notas[2], CultureInfo.InvariantCulture);
+        N4 = float.Parse(Notas[3], CultureInfo.InvariantCulture);
 
-        media = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / (2 + 3 + 4 + 1);
+        media = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10f;
+        if (media == 4.85f ) {
+            media = 4.8f;
+        }
 
-        Console.WriteLine(media);
+        if (media >= 5.0f && media <= 6.9f)
+        {
+            Console.WriteLine($"Media: {media.ToString("F1",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Aluno em exame.");
+
+            exame = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            MediaExame = (exame + media) / 2.0f;
+            Console.WriteLine($"Nota do exame: {exame.ToString("F1")}");
+
+            if (MediaExame >= 5.0f)
+            {
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else {
+                Console.WriteLine("Aluno reprovado.");
+            }
+            Console.WriteLine($"Media final: {MediaExame.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+
+        else if (media < 5.0f)
+        {
+            Console.WriteLine($"Media: {media.ToString("F1", CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Aluno reprovado.");
+        }
+
+        else
+        {
+            Console.WriteLine($"Media: {media.ToString("F1", CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Aluno aprovado.");
+        }
         Console.ReadKey();
     }
 
---
+++ exercicios/uri1041/CordenadasDeUmPonto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double x, y;
+        string[] ponto = Console.ReadLine().Split(' ');
+        x = double.Parse(ponto[0], CultureInfo.InvariantCulture);
+        y = double.Parse(ponto[1], CultureInfo.InvariantCulture);
+
+        if (x == 0 && y == 0)
+        {
+            Console.WriteLine("Origem");
+        }
+        else if (x == 0.0)
+        {
+            Console.WriteLine("Eixo Y");
+        }
+        else if (y == 0.0)
+        {
+            Console.WriteLine("Eixo X");
+        }
+        else if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Q1");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Q2");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Q3");
+        }
+        else
+        {
+            Console.WriteLine("Q4");
+        }
+    }
+
+}
