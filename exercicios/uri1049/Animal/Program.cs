2a0e812fd8ed849b5b02e25b4002b9d841d6c812
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-06 20:22:17 -0300

    Uri1049

---
+++ exercicios/uri1049/Animal/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string a, b, c;
+        a = Console.ReadLine();
+        b = Console.ReadLine();
+        c = Console.ReadLine();
+        switch (a)
+        {
+            case "vertebrado":
+                if (b == "ave")
+                {
+                    if (c == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else
+                {
+                    if (c == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+                break;
+
+            case "invertebrado":
+                if (b == "inseto")
+                {
+                    if (c == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else
+                {
+                    if (c == "onivoro")
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                    else
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                }
+                break;
+        }Console.ReadKey();
+    }
+}
