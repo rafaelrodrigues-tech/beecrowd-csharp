8126c486fb9a28d0d790313b09a4d5460978a627
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-09 21:31:40 -0300

    adiciona exercicio 1051

---
+++ exercicios/uri1051/ImpostoDeRenda/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double renda;
+        double imposto = 0.0;
+        renda = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        if (renda <= 2000.00)
+        {
+            Console.WriteLine("Isento");
+        }
+        else
+        {
+            if (renda > 2000.00 && renda <= 3000.00)
+            {
+                imposto = (renda - 2000.00) * 0.08;
+            }
+            else if (renda > 3000.00 && renda <= 4500.00)
+            {
+                imposto = (1000.00 * 0.08) + ((renda - 3000.00) * 0.18);
+            }
+            else
+            {
+                imposto = (1000.00 * 0.08) + (1500.00 * 0.18) + ((renda - 4500.00) * 0.28);
+            }
+            Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+        }   Console.ReadKey();
+    }
+}
