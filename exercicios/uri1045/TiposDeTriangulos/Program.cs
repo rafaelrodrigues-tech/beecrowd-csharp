73009c8fc33d881ea954d743898f2973355067a5
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-10 21:18:26 -0300

    Adiciona exercicio 1045

--- exercicios/uri1045/TiposDeTriangulos/Program.cs
+++ exercicios/uri1045/TiposDeTriangulos/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Globalization;
 
 class URI
 {
 
     static void Main(string[] args)
     {
-        
+        double a, b, c;
+        string[] vet = Console.ReadLine().Split(' ');
+        a = double.Parse(vet[0],CultureInfo.InvariantCulture);
+        b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+        c = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
+        double[] lados = {a, b, c};
+        Array.Sort(lados);
+        Array.Reverse(lados);
+
+        a = lados[0];
+        b = lados[1];
+        c = lados[2];
+
+        if (a >= b + c)
+        {
+            Console.WriteLine("NAO FORMA TRIANGULO");
+            return;
+            Console.ReadKey();
+        }
+        if (a * a == b * b + c * c)
+        {
+            Console.WriteLine("TRIANGULO RETANGULO");
+        }
+        if (a * a > (b * b + c * c))
+        {
+            Console.WriteLine("TRIANGULO OBTUSANGULO");
+        }
+        if (a * a < b * b + c * c)
+        {
+            Console.WriteLine("TRIANGULO ACUTANGULO");
+        }
+        if (a == b && b == c)
+        {
+            Console.WriteLine("TRIANGULO EQUILATERO");
+        }
+        if ((a == b && a != c) || ( a == c && a != b) || (b == c && b != a))
+        {
+            Console.WriteLine("TRIANGULO ISOSCELES");
+        }
+        Console.ReadKey();
     }
-
 }
