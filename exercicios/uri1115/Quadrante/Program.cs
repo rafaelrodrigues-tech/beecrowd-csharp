837c4254a220f5e64754f30d6c0144f758121d8d
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-15 21:37:03 -0300

    Exercicio uri1115

---
+++ exercicios/uri1115/Quadrante/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int x, y;
+        string[] vetor = Console.ReadLine().Split(' ');
+        x = int.Parse(vetor[0]);
+        y = int.Parse(vetor[1]);
+
+        while (x != 0 && y != 0)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("primeiro");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("segundo");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("terceiro");
+            }
+            else
+            {
+                Console.WriteLine("quarto");
+            }
+            vetor = Console.ReadLine().Split(' ');
+            x = int.Parse(vetor[0]);
+            y = int.Parse(vetor[1]);
+        }
+        Console.ReadLine();
+    }
+
+}
