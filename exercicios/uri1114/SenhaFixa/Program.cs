e12f8eb46aec3f6c380f7bb32f16fb70c384a84d
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-15 21:04:12 -0300

    Adiciona Exercicio 1114

---
+++ exercicios/uri1114/SenhaFixa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int senha;
+        int senhaValida = 2002;
+        senha = int.Parse(Console.ReadLine());
+
+        while (senha != senhaValida)
+        {
+            Console.WriteLine("Senha Invalida");
+            senha = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Acesso Permitido");
+        Console.ReadLine();
+    }
+}
