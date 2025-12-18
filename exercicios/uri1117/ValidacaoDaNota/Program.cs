0cbfc8e8e6b348707d5407d74e22e6786bdf17d5
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-17 21:54:55 -0300

    Exercicio, Validação de nota

---
+++ exercicios/uri1117/ValidacaoDaNota/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double Nota, Media, Soma, Cont;
+
+        Soma = 0;
+        Cont = 0;
+        while (Cont < 2)
+        {
+            Nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (Nota < 0.0 || Nota > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+            }
+            else {
+                Soma += Nota;
+                Cont++;
+            }
+        }
+        Media = Soma / Cont;
+        Console.WriteLine($"media = {Media.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.ReadLine();
+    }
+}
