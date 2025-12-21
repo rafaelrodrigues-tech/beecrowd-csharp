1ae11fe83e5edfa576996ecb056cb5d4b0a53900
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-21 16:28:06 -0300

    feat: solution to age calculation exercises

---
+++ exercicios/uri1154/Idades/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int idade;
+        int cont = 0;
+        int soma = 0;
+        double media;
+
+        idade = int.Parse(Console.ReadLine());
+
+        while (idade >= 0)
+        {
+            cont++;
+            soma += idade;
+            idade = int.Parse(Console.ReadLine());
+        }
+
+        media = (double)soma / cont;
+        Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
