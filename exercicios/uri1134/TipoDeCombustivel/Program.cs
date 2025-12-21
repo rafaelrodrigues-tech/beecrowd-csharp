fe170a9a84a7cdd6e4100e8965b7a67a587fb193
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-21 15:01:18 -0300

    feat: resolve fuel survey exercise

---
+++ exercicios/uri1134/TipoDeCombustivel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int opcao,alcool, gas, diesel;
+        opcao = int.Parse(Console.ReadLine());
+
+        alcool = gas = diesel = 0;
+
+        while (opcao != 4)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    {
+                        alcool += 1;
+                        opcao = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                case 2:
+                    {
+                        gas += 1;
+                        opcao = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                case 3:
+                    {
+                        diesel += 1;
+                        opcao = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                default:
+                    {
+                        opcao = int.Parse(Console.ReadLine());
+                        break;
+                    }
+            }
+        }
+        Console.WriteLine("MUITO OBRIGADO");
+        Console.WriteLine($"Alcool: {alcool}");
+        Console.WriteLine($"Gasolina: {gas}");
+        Console.WriteLine($"Diesel: {diesel}");
+    }
+}
