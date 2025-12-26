835844bd57b58b0ac2328c7cecca45839eed2d93
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-26 12:03:24 -0300

    feat: resolve exercício de repetição com do-while

---
+++ exercicios/uri1118/VariasNotasComValidacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int opcao = 1;
+
+        while (opcao == 1)
+        {
+            double soma = 0;
+            int cont = 0;
+
+            while (cont < 2)
+            {
+                double nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (nota < 0.0 || nota > 10.00)
+                {
+                    Console.WriteLine("nota invalida");
+                }
+                else
+                {
+                    soma += nota;
+                    cont++;
+                }
+            }
+                double media = soma / 2;
+                Console.WriteLine($"media = {media.ToString("F2",CultureInfo.InvariantCulture)}");
+
+                do
+                {
+                Console.WriteLine("novo calculo (1-sim 2-nao)");
+                opcao = int.Parse(Console.ReadLine());
+                }
+                while (opcao != 1 && opcao != 2);
+        }
+    }
+}
