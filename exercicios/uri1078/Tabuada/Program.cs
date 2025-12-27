230412fb22723e43bcc6ba3a20e04bf822ea30dd
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-27 12:17:53 -0300

    resolve exercicio estrutura repetitiva FOR

---
+++ exercicios/uri1078/Tabuada/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int N;
+        N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine(i +" x "+ N +" = "+ i*N);
+        }
+        Console.ReadLine();
+    }
+
+}
--- exercicios/uri1131/Grenais/Program.cs
+++ exercicios/uri1131/Grenais/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using static System.Net.Mime.MediaTypeNames;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int inter, gremio;
+        int empates = 0;
+        int VitoriasInter = 0;
+        int VitoriasGremio = 0;
+        int total = 0;
+        int resposta;
+        do
+        {
+            string[] grenal = Console.ReadLine().Split(' ');
+            inter = int.Parse(grenal[0]);
+            gremio = int.Parse(grenal[1]);
+
+            total++;
+
+            if (inter > gremio)
+            {
+                VitoriasInter++;
+            }
+            else if (inter < gremio)
+            {
+                VitoriasGremio++;
+            }
+            else
+            {
+                empates++;
+            }
+            Console.WriteLine("Novo grenal (1-sim 2-nao)");
+            resposta = int.Parse(Console.ReadLine());
+        }
+        while (resposta == 1);
+        {
+            Console.WriteLine($"{total} grenais");
+            Console.WriteLine($"Inter:{VitoriasInter}");
+            Console.WriteLine($"Gremio:{VitoriasGremio}");
+            Console.WriteLine($"Empates:{empates}");
+
+            if (VitoriasInter > VitoriasGremio)
+            {
+                Console.WriteLine("Inter venceu mais");
+            }
+            else if (VitoriasInter < VitoriasGremio)
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }
+            else
+            {
+                Console.WriteLine("Nao houve vencedor");
+            }
+        }
+    }
+}
