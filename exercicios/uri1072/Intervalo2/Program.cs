b559a502422f055756324c70253f6661bdb5bfac
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-28 11:37:04 -0300

    feat: resolve intervalo entre numeros

---
+++ exercicios/uri1072/Intervalo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int N, X;
+        int ContIn = 0;
+        int ContOut = 0;
+        N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++) 
+        {
+            X = int.Parse(Console.ReadLine());
+
+            if (X >= 10 && X <= 20)
+            {
+                ContIn++;
+            }
+            else
+            {
+                ContOut++;   
+            }
+        }
+        Console.WriteLine(ContIn +" in");
+        Console.WriteLine(ContOut +" out");
+        Console.ReadLine();
+    }
+}
