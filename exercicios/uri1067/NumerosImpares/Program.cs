2616f0b70307db8d14b403ac05dcf657a9a11e43
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-28 11:11:20 -0300

    feat: resolve Exercise to find odd numbers.

---
+++ exercicios/uri1067/NumerosImpares/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int X;
+        X = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= X; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        Console.ReadLine();
+    }
+}
