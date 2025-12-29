fa161e9834a736f0834bd393e6c53c5f387bc66d
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-29 20:24:46 -0300

    feat: resolve exercise Par or Impar

---
+++ exercicios/uri1074/ParOuImpar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int N, x;
+        N = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= N; i++)
+        {
+            x = int.Parse(Console.ReadLine());
+
+            if (x == 0)
+            {
+                Console.WriteLine("NULL");
+            }
+            else if (x % 2 == 0)
+            {
+                if (x > 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+                else
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+            }
+            else {
+                if (x > 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+                else
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+            }
+        }
+    }
+}
