fa7d6a18d74c134cce87283419c91bea20a65bdc
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-12-30 05:35:21 -0300

    feat: resolve exercise uri1079

---
+++ exercicios/uri1079/MediasPonderadas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int N;
+        double notaA,notaB,notaC, mediaPonderada;
+        N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+
+            string[] media = Console.ReadLine().Split(' ');
+            notaA = double.Parse(media[0],CultureInfo.InvariantCulture);
+            notaB = double.Parse(media[1],CultureInfo.InvariantCulture);
+            notaC = double.Parse(media[2],CultureInfo.InvariantCulture);
+
+            mediaPonderada = ((notaA * 2) + (notaB * 3) + (notaC * 5)) / (2 + 3 + 5);
+            Console.WriteLine(mediaPonderada.ToString("F1"),CultureInfo.InvariantCulture);
+
+        }Console.ReadLine();
+
+    }
+
+}
