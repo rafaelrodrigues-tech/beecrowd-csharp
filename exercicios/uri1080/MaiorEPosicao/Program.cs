688c2a9f44a1b5b7f1ee76a96f587a1ea8528a27
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-01 10:28:33 -0300

    feat: resolve Big and Index uri1080

---
+++ exercicios/uri1080/MaiorEPosicao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int num;
+        int maior = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= 100; i++)
+        {
+            num = int.Parse(Console.ReadLine());
+            
+            if (num > maior)
+            {
+                maior = num;
+            }
+
+        }
+        Console.WriteLine(maior);
+        Console.ReadLine();
+
+    }
+
+}
