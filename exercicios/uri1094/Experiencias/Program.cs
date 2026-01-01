28d858bbd4d32d61d041b98a6e2a314295b44968
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-01 11:27:01 -0300

    uri1094

---
+++ exercicios/uri1094/Experiencias/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string quantia;
+        string tipo;
+
+        string[] cobaias = Console.ReadLine().Split(' ');
+        quantia = cobaias[0];
+        tipo = cobaias[0];
+
+        Console.WriteLine(cobaias);
+        Console.ReadLine();
+        
+    }
+
+}
