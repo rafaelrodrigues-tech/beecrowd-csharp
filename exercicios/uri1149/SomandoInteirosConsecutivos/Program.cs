85533235fa253d80c80e71dfc1c4701fe05fdcd9
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-05 21:55:47 -0300

    resolve exercise uri1149

---
+++ exercicios/uri1149/SomandoInteirosConsecutivos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A, N;
+        string[] valores = Console.ReadLine().Split(' ');
+
+        A = int.Parse(valores[0]);
+        N = int.Parse(valores[1]);
+
+        while (N <= 0)
+        {
+            valores = Console.ReadLine().Split(' ');
+            A = int.Parse(valores[0]);
+            N = int.Parse(valores[1]);
+        }
+
+        int soma = 0;
+        for (int i = A; i <= A+N-1;i++)
+        {
+            soma += i;
+        }
+        Console.WriteLine(soma);
+        Console.ReadLine();
+    }
+}
