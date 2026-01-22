c278f079e52af57cf3737d7cd6f1a105ef03c2ce
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-22 20:27:22 -0300

    Matriz

---
+++ exercicios/Matriz/Ex1/ValoresNegativos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int M;
+            int N;
+            int[,] A;
+
+            string[] MN = Console.ReadLine().Split(' ');
+            M = int.Parse(MN[0]);
+            N = int.Parse(MN[1]);
+            A = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; i++)
+                {
+                    A[M, N] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
