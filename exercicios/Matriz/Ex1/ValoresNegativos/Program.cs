ffa28724e9c10200a2f7e4e11f4ee26373cd3977
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-23 18:04:15 -0300

    fix: corrige valores na matriz

--- exercicios/Matriz/Ex1/ValoresNegativos/Program.cs
+++ exercicios/Matriz/Ex1/ValoresNegativos/Program.cs
@@ -6,8 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int M;
-            int N;
+            int M,N;
             int[,] A;
 
             string[] MN = Console.ReadLine().Split(' ');
@@ -18,14 +17,23 @@ namespace MyApp
             for (int i = 0; i < M; i++)
             {
                 string[] s = Console.ReadLine().Split(' ');
-                for (int j = 0; j < N; i++)
+                for (int j = 0; j < N; j++)
                 {
-                    A[M, N] = int.Parse(Console.ReadLine());
+                    A[i, j] = int.Parse(s[j]);
                 }
             }
-
+            Console.WriteLine("VALORES NEGATIVOS:");
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (A[i, j] < 0)
+                    {
+                        Console.WriteLine(A[i,j]);
+                    }
+                }            
+            }
             Console.ReadLine();
-
         }
     }
 }
