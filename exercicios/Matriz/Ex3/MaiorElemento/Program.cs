c5da3a10155db98632f57ac827aa58fdf890b567
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-24 10:46:06 -0300

    feat: find max value in eath matriz row

---
+++ exercicios/Matriz/Ex3/MaiorElemento/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[,] A;
+            N = int.Parse(Console.ReadLine());
+            A = new int[N,N];
+
+            for (int i = 0; i < N; i++) {
+                string[] linha = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    A[i, j] = int.Parse(linha[j]);
+                }
+            }
+            int[] mat = new int[N];
+            for (int i = 0; i < N; i++) {
+                int maior = 0;
+                for (int j = 0; j < N; j++) {
+                    if (A[i, j] > maior) {
+                        maior = A[i, j];
+                    }
+                    mat[i] = maior;
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine(mat[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
