340f014e5a45f9f5b61e8408239933eb19a3b238
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-24 10:15:25 -0300

    feat: add sum of matriz rows

---
+++ exercicios/Matriz/Ex2/SomaInteiros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace TourOfCsharp;
+
+class Program
+{
+    static void Main()
+    {
+        int N;
+        int[,] A;
+
+        N = int.Parse(Console.ReadLine());
+        A = new int[N,N];
+
+        for (int i = 0; i < N; i++){
+            string[] l = Console.ReadLine().Split(' ');
+            for (int j = 0; j < N; j++){
+                A[i, j] = int.Parse(l[j]);
+            }
+        }
+        int[] vet = new int[N];
+        for (int i = 0; i < N; i++){
+            int soma = 0;
+            for (int j = 0; j < N; j++) {
+                soma = soma + A[i,j];
+            }
+            vet[i] = soma;
+        }
+        for (int i = 0; i < N; i++) {
+                Console.WriteLine(vet[i]);
+        } 
+        Console.ReadLine();
+    }
+}
