b478f08d89b7b88d2ea093b5912ac351d13a6c94
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-25 10:23:37 -0300

    feat: resolve sum of above the diagonal

---
+++ exercicios/Matriz/Ex4/SomaAcimaDiagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[,] mat;
+
+            N = int.Parse(Console.ReadLine());
+            mat = new int[N, N];
+
+            for (int i = 0; i < N; i++) {
+                string[] l = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    mat[i, j] = int.Parse(l[j]);
+                }
+            }
+            int soma = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (j > i)
+                    {
+                        soma += mat[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
