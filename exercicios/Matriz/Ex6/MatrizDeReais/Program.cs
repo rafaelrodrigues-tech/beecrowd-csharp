7daea7b659f0123ff0901ccc14d1423f7aa40ca6
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-01-27 21:54:58 -0300

    Matriz de números reais

---
+++ exercicios/Matriz/Ex6/MatrizDeReais/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+namespace TourOfCsharp;
+
+class Program
+{
+    static void Main()
+    {
+         
+        int N;
+        double[,] mat;
+
+        N = int.Parse(Console.ReadLine());
+        mat = new double[N,N];
+       //A
+        for (int i = 0; i < N; i++) {
+            string[] l = Console.ReadLine().Split(' ');
+            for (int j = 0; j < N; j++) {
+                mat[i, j] = double.Parse(l[j],CultureInfo.InvariantCulture);
+            }
+        }
+        //B
+        double soma = 0.0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (mat[i, j] > 0.0)
+                {
+                    soma = soma + mat[i, j]; 
+                }
+            }
+        }
+        Console.WriteLine(soma.ToString("F1", CultureInfo.InvariantCulture));
+
+        
+        int indice = int.Parse(Console.ReadLine());
+        for (int j = 0; j < N; j++)
+          {
+            Console.Write(mat[indice,j]+" ");
+
+          }
+        Console.WriteLine();
+
+        int coluna = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++) {
+            Console.Write(mat[i,coluna]+ " ");
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < N; i++) {
+                Console.Write(mat[i, i]+" ");
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (mat[i, j] < 0.0)
+                {
+                    mat[i,j] = mat[i,j] * mat[i, j];
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                Console.Write(mat[i,j]+" ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
