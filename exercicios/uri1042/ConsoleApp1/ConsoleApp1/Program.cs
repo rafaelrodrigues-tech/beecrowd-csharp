d5812ab9f89378be31d4788ebc4755f7ad27d7de
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-04-14 21:07:18 -0300

    feat: resolve logical exercises

---
+++ exercicios/uri1042/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            int c = int.Parse(line[2]);
+            if (a < b && b < c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+            }
+            else if (b < a && a < c)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+            }
+            else if (a > c && b > a)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+            }
+            
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(line[i]);
+            }
+        }
+    }
+}
