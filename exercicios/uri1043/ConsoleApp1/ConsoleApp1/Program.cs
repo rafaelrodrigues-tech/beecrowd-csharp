1f2c34bdf4841302c46680bffb57de6f7db46d6a
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-04-15 07:50:32 -0300

    resolve exercise 1043

---
+++ exercicios/uri1043/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+            double a = double.Parse(line[0],CultureInfo.InvariantCulture);
+            double b = double.Parse(line[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(line[2], CultureInfo.InvariantCulture);
+
+            double area = ((a + b) * c )/ 2.0;
+            double perimetro = a + b + c;
+
+            if (b + c == a)
+            {
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
