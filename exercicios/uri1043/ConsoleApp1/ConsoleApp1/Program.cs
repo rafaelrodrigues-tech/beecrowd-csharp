9ed76f46cd49aa978f6024508245a5dc2ff2f5e8
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-04-17 07:50:17 -0300

    atualiza uri1043

--- exercicios/uri1043/ConsoleApp1/ConsoleApp1/Program.cs
+++ exercicios/uri1043/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,20 +8,21 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string[] line = Console.ReadLine().Split();
-            double a = double.Parse(line[0],CultureInfo.InvariantCulture);
+            double a = double.Parse(line[0], CultureInfo.InvariantCulture);
             double b = double.Parse(line[1], CultureInfo.InvariantCulture);
             double c = double.Parse(line[2], CultureInfo.InvariantCulture);
 
-            double area = ((a + b) * c )/ 2.0;
+            double area = ((a + b) * c) / 2.0;
             double perimetro = a + b + c;
 
-            if (b + c == a)
-            {
-                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
-            }
-            else
+            if (a + b > c && a + c > b && b + c > a)
             {
                 Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+            else
+            {
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
             }
         }
     }
