08ce34c5fd03ac5909d912ffcd053f57eb4a1c55
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-04-23 09:02:43 -0300

    exercises 1060 and 1064

---
+++ exercicios/uri1060/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int positive = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                double values = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);//vai usar o ponto (padrão internacional)
+                if (values >= 0.0)
+                {
+                    positive++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(positive + " valores positivos");
+        }
+    }
+}
---
+++ exercicios/uri1064/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int positive = 0;
+            double avg = 0.0;
+            double sum = 0.0;
+            for (int i = 1; i <= 6; i++)
+            {
+                double values = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                if (values >= 0.0)
+                {
+                    positive++;
+                    sum += values;
+                }
+            }
+            avg = sum / positive;
+            Console.WriteLine(positive + " valores positivos");
+            Console.WriteLine(avg.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
