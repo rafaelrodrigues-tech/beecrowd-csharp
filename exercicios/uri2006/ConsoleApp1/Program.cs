78ebc7caef4d166470b73e0d398012548aa47613
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-04-28 20:00:46 -0300

    identificando o chá

---
+++ exercicios/uri2006/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            int cont = 0;
+
+            string[] linha = Console.ReadLine().Split(' ');
+            int a = int.Parse(linha[0]);
+            int b = int.Parse(linha[1]);
+            int c = int.Parse(linha[2]);
+            int d = int.Parse(linha[3]);
+
+            foreach (string num in linha)
+            {
+                if (num == n)
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine(cont);
+        }
+    }
+}
