38abb71cb579c53c04f11aa88e19332e642d0b12
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-04-29 21:55:24 -0300

    Busca na Internet

---
+++ exercicios/uri2413/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            int num = (t * 2) * 2;
+            Console.WriteLine(num);
+        }
+    }
+}
