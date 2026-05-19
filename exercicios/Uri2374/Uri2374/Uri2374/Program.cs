21fad5d581083784d7a32cf59ff26ad73e658670
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-19 11:14:43 -0300

    Resolucao dos exercicios uri2374 e uri3048

---
+++ exercicios/Uri2374/Uri2374/Uri2374/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace consoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            int subtracao = n - m;
+            Console.WriteLine(subtracao);
+        }
+    }
+}
---
+++ exercicios/uri3048/SequenciaSecreta/SequenciaSecreta/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SequenciaSecreta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> v = new List<int>(n);
+
+            for (int i = 1; i <= n; i++)
+            {
+                v.Add(int.Parse(Console.ReadLine()));
+
+            }
+            int cont = 1;
+
+            for (int i = 1; i<= n-1; i++)
+            {
+                if (v[i] != v[i-1])
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine(cont);
+        }
+    }
+}
