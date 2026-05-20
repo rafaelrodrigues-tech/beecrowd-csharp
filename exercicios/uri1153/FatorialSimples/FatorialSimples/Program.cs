e6dd3a3c510b0b92eceb0951ee2a9c4a8aaa187b
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-20 11:42:27 -0300

    Resolucao exercicio fatorial simples

---
+++ exercicios/uri1153/FatorialSimples/FatorialSimples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FatorialSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                n *= i;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
