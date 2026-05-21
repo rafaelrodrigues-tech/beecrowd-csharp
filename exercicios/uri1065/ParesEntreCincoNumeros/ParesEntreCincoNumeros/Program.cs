cd85ec845dc3c1a3920e6bdf1f4639d7989b1684
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-20 21:25:55 -0300

    Exercicio simples: encontrando valores pares

---
+++ exercicios/uri1065/ParesEntreCincoNumeros/ParesEntreCincoNumeros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                int numero = int.Parse(Console.ReadLine());
+                if (numero % 2 == 0)
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine(cont + " valores pares");
+        }
+    }
+}
