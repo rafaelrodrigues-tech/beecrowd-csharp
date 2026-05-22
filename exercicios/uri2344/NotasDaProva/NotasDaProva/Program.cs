5208cf11418ffae19a8f22115723056ae0cd4e8f
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-22 09:41:10 -0300

    Resolucao : Notas de A a E

---
+++ exercicios/uri2344/NotasDaProva/NotasDaProva/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("E");
+
+            }
+            else if (n < 36)
+            {
+                Console.WriteLine("D");
+            }
+            else if (n < 61)
+            {
+                Console.WriteLine("C");
+            }
+            else if (n < 86)
+            {
+                Console.WriteLine("B");
+            }
+            else {
+                Console.WriteLine("A");
+            }
+        }
+    }
+}
