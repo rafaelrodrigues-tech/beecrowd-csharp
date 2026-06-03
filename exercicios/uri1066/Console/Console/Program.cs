0c13ae090ae61506fa145ba92cb9aaadc9d2aeb7
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-06-03 20:26:20 -0300

    Exercicios resolvidos 1066 e 2344

---
+++ exercicios/uri1066/Console/Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int par = 0;
+            int impar = 0;
+            int pos = 0;
+            int neg = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x % 2 == 0)
+                {
+                    par++;
+                    if (x > 0)
+                    {
+                        pos++;
+                    }
+                    if (x < 0)
+                    {
+                        neg++;
+                    }
+                }
+                else
+                {
+                    impar++;
+                    if (x > 0)
+                    {
+                        pos++;
+                    }
+                    if (x < 0)
+                    {
+                        neg++;
+                    }
+                }
+            }
+            Console.WriteLine($"{par} valor (es) par (es)");
+            Console.WriteLine($"{impar} valor (es) impar (es)");
+            Console.WriteLine($"{pos} valor (es) positivo (s)");
+            Console.WriteLine($"{neg} valor (es) negativo (s)");
+        }
+    }
+}
--- exercicios/uri2344/NotasDaProva/NotasDaProva/Program.cs
+++ exercicios/uri2344/NotasDaProva/NotasDaProva/Program.cs
@@ -2,7 +2,7 @@
 
 namespace MyApp
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
