724f84e64ce37720c82db3b253b58a37688e39b5
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-06-14 20:19:57 -0300

    Resolve: Uri2936

---
+++ exercicios/uri2936/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 300, 1500,600,1000,150};
+            int chica = 225;
+            int consumo = 0;
+            for (int i = 0; i <=4; i++)
+            {
+                int qtdd = int.Parse(Console.ReadLine());
+                consumo = qtdd * numeros[i];
+                chica += consumo;
+            }
+            Console.Write(chica);
+        }
+    } 
+}
