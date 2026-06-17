7c8371627907fbee93d2ecf19d505b13ef07f460
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-06-16 22:01:14 -0300

    Numeros impares seguidos

---
+++ exercicios/uri1070/SeisNúmerosÍmpares/SeisNúmerosÍmpares/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        if (x % 2 == 0)
+        {
+            x++;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            if (x % 2 ==1)
+            {
+                Console.WriteLine(x);
+            }
+            x+=2;
+        }
+
+    }
+
+}
