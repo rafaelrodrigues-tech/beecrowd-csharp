68acafaf2844f76652580d453037afa5c2147497
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-07 10:37:34 -0300

    feat: resolve exercici raio

---
+++ exercicios/uri1589/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class URI
+{   
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int soma = 0;
+        List<int> numb = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(" ");
+            int r1 = int.Parse(numbers[0]);
+            int r2 = int.Parse(numbers[1]);
+            numb.Add(soma = r1 + r2);
+        }
+        foreach (int x in numb) Console.WriteLine(x); 
+    }
+}
