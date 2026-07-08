c43539078c2b59cc5187bb5d57335e02fe44a9cb
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-08 10:23:36 -0300

    resolucao exercicio quadrado de pares

--- exercicios/uri1073/QuadroDePares/Program.cs
+++ exercicios/uri1073/QuadroDePares/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                double pares = Math.Pow(i, 2);
+                Console.WriteLine($"{i}^{2} = {pares}");
+            }
+        }
+    }
+}
