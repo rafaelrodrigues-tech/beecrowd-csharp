464664ea0f8ac93d62813ef8cbc946eefb9141f1
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-08 10:37:23 -0300

    resolucao resto igual a 2

---
+++ exercicios/uri1075/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i % n == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
