b5d4cf2e78e3e759763a74a04ec6c079b91e7954
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-09 17:41:04 -0300

    Resolucao exercicio 1095

---
+++ exercicios/uri1095/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {/*
+        for (int i = 1; i <= 60; i += 3)
+        {
+            Console.WriteLine($"I={i}");
+
+            for (int j = 60; j >= 0; j -= 5)
+            {
+                Console.WriteLine($"J={j}");
+            }
+
+        }
+        */
+        int i = 1;
+        int j = 60;
+        while (i <= 60 && j >= 0)
+        {
+            Console.WriteLine($"I={i} J={j}");
+            i += 3;
+            j -= 5;
+            
+        }
+    }
+}
