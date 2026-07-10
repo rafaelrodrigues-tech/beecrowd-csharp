97ad247da3c0ebe526cae9faf7fca5e5c2ee1a29
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-10 20:28:47 -0300

    Resolucao exercicio uri1116 dividindo x/y

---
+++ exercicios/uri1116/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            int x = int.Parse(numbers[0]);
+            int y = int.Parse(numbers[1]);
+            if (y == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                double div = (double)x / (double)y;
+                Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
