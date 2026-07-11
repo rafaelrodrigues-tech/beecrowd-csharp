9f6c2d5b2e4d22fd91ad2922bf54874c0330e717
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-10 21:21:13 -0300

    exercicio soma dos nao divisores de 13

---
+++ exercicios/uri1132/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int menor = x;
+        int maior = y;
+        if (y < x)
+        {
+            menor = y;
+            maior = x;
+        }
+        int soma = 0;
+        for (int i = menor; i <= maior; i++)
+        {
+            if (i % 13 != 0)
+            {
+                soma += i;
+            }
+        }
+        Console.WriteLine(soma);
+    }
+}
