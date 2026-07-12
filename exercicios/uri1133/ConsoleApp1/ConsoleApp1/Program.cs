7253e4b7ad214b07573569ae9ac9a56c745870d1
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-12 14:49:37 -0300

    feat: resolucao exercicio resto da divisao de 5 == 2

---
+++ exercicios/uri1133/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int maior = y;
+        int menor = x;
+        if (y < x)
+        { 
+            menor = y;
+            maior = x;
+        }
+        for (int i = menor + 1; i < maior; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            { 
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+
+}
