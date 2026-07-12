b17b1052cf6fc729050e28eb22e6b8357faa95f6
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-12 20:01:23 -0300

    feat: resolve exercicio uri1142

---
+++ exercicios/uri1142/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        int i = 1;
+        int linha = 1;
+        while (linha <= n)
+        {
+            if (!(i % 4 == 0))
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            else
+            {
+                linha++;
+                Console.Write("PUM\n");
+                i++;
+            }
+        }
+    }
+}
