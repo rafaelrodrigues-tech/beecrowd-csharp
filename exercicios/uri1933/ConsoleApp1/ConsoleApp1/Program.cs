2db1c6012288c0032c3a0bcfb0a4fa114b893d6d
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-13 09:42:34 -0300

    feat: exercicio adivinhando carta da vitoria(TRI-DU)

---
+++ exercicios/uri1933/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        string[] cartas = Console.ReadLine().Split(' ');
+        int a = int.Parse(cartas[0]);
+        int b = int.Parse(cartas[1]);
+
+        if (a == b) Console.WriteLine(a);
+
+        else if (a > b) Console.WriteLine(a);
+
+        else Console.WriteLine(b);
+    }
+}
