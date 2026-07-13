d79c8f1ed66981212ac538292b860729ce58268b
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-13 09:51:54 -0300

    feat: Encontrando quadrado e cubo de valor 'i'

---
+++ exercicios/uri1143/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            double quadrado = Math.Pow(i,2);
+            double cubo = Math.Pow(i,3);
+            Console.WriteLine($"{i} {quadrado} {cubo}");
+        }
+    }
+}
