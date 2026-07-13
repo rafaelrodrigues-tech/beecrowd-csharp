ff5a5ee6ac2384bdbce62cdcb1110acc5e216765
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-13 10:40:58 -0300

    feat: resolucao Sequencia IJ3

---
+++ exercicios/uri1097/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        int j = 7;
+        while (i < 11)
+        {
+            if (!(j - i == 3))
+            {
+                Console.WriteLine($"I={i} J={j}");
+                j -= 1;
+            }
+            else{
+                i += 2;
+                j += 5;
+            }
+        }
+    }
+}
