6531f59bc830fb0d0f11429ff5d64293979cba8c
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-15 14:44:41 -0300

    feat: Resolve imprimir sequencias crescentes

---
+++ exercicios/uri1146/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        while (x > 0)
+        {
+            int i = 1;
+            while (i <= x)
+            { 
+                Console.Write(i+" ");
+                i++;
+            }
+            Console.WriteLine();
+            x = int.Parse(Console.ReadLine());
+        }
+    }
+}
