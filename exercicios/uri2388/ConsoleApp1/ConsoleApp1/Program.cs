dd2f0196295b2bafb3641377633904039e7cf727
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-20 20:46:51 -0300

    feat: resolve tacografo exercise

---
+++ exercicios/uri2388/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int distancia = 0;
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            int tempo = int.Parse(numbers[0]);
+            int velocidade = int.Parse(numbers[1]);
+            distancia += tempo * velocidade ;
+        }
+        Console.WriteLine(distancia);
+    }
+}
