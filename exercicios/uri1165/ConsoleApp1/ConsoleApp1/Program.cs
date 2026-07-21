d45ce105cbaaee2e234c4c4103a30538bcfc2804
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-21 08:45:42 -0300

    feat: exercicio encontrando numeros primos

---
+++ exercicios/uri1165/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int cont = 0;
+            for (int j = 1; j <= x; j++)
+            {
+                if (x % j == 0)
+                {
+                    cont++;
+                }
+            }
+            if (cont == 2)
+            {
+                Console.WriteLine($"{x} eh primo");
+            }
+            else
+            {
+                Console.WriteLine($"{x} nao eh primo");
+            }
+        }
+    }
+}
