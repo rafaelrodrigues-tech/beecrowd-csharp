106474052ad9e4c72f80dd86af9a5304ecc4963d
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-21 09:26:20 -0300

    feat: listando valores pares em ordem crescente  e impares decrescente

---
+++ exercicios/04 - estruturas e biblioteca/uri1259/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        List<int> pares = new List<int>();
+        List<int> impares = new List<int>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) pares.Add(x);
+
+            else impares.Add(x);
+        }
+        foreach (int p in pares.Order())
+        {
+            Console.WriteLine(p);
+        }
+        foreach (int i in impares.OrderDescending())
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
