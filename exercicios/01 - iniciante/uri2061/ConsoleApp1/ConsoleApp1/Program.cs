1303ba9495c614e2a792974a0725d6ef7d0d0339
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-22 16:10:33 -0300

    feat: exercicio encontrando total de abas abertas e fechadas

---
+++ exercicios/01 - iniciante/uri2061/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string[] abas = Console.ReadLine().Split(' ');
+        int n = int.Parse(abas[0]);
+        int m = int.Parse(abas[1]);
+        for (int i = 1; i <= m; i++)
+        { 
+            string resultado = Console.ReadLine();
+            if (resultado == "fechou")
+            {
+                n = (n - 1) + 2;
+            }
+            else
+            {
+                n -= 1;
+            }
+        }
+        Console.WriteLine(n);
+
+    }
+
+}
