10bc3d0073c3f7b80ad27e265f996258078a1e65
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-23 11:32:55 -0300

    feat: resolve exercise easy Fibonacci

---
+++ exercicios/01 - iniciante/uri1151/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int a = 0;
+        int b = 1;
+        if (n == 1)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+        Console.Write(a + " "+ b);
+        for (int i = 3; i <= n; i++)
+        {
+            int c = a + b;
+            Console.Write(" " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
