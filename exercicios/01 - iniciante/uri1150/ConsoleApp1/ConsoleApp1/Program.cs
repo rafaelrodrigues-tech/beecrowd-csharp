ff10ce93e87e9c40f41f73a0824908e9cc5dc2e8
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-08-04 10:41:07 -0300

    feat:resolve Squecencia IJ4

---
+++ exercicios/01 - iniciante/uri1150/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int x = int.Parse(Console.ReadLine());//21
+        int y = int.Parse(Console.ReadLine());//21
+        int valores = 1;
+        while (x >= y)
+        {
+            y = int.Parse(Console.ReadLine()); //15
+            if (y > x)
+            {
+                while (valores <= y)
+                { 
+                    
+                }
+            }
+        }
+        Console.WriteLine(valores);
+    }
+}
